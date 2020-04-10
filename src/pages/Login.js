4eaeb82d478087719e0b36dcb2baa31794abcf52
Nilson28 <nilson.cardona@correounivalle.edu.co>
2020-04-09 20:33:03 -0500

    initial commit

---
+++ src/pages/Login.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import './Login.css';
+
+export default class Login extends Component {
+
+    state = {
+        correo: '',
+        cedula: ''
+    }
+
+    onSubmit = (event) => {
+        this.crearUsuario(this.state.correo, parseInt(this.state.cedula, 10));
+        event.preventDefault();
+    }
+
+    onChange = (event) => {
+        this.setState({
+           [event.target.name] : event.target.value,
+
+        });
+    }
+
+    crearUsuario(correo, cedula) {
+        const mensaje = {
+            correo,
+            cedula
+        }
+        axios.post('https://server-97.herokuapp.com/api/estudiantes/Login', mensaje)
+        .then((res) => {
+            console.log(res.data)})
+    }
+
+    
+
+    render() {
+        return (
+            <div className="contenedor0">
+                <div className="contenedor1">
+
+                </div>
+                <div className="contenedor2">
+                    <form className = "form" onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <i className="medium material-icons">chrome_reader_mode</i>
+                            <input onChange={this.onChange} type="text" className="form-control" id="formGroupExampleInput" placeholder="identificacion" value={this.state.correo} name="correo"/>
+                        </div>
+                        <div className="form-group">
+                            <i className="medium material-icons">account_box</i>
+                            <input onChange={this.onChange} type="password" className="form-control" id="formGroupExampleInput2" placeholder="Nombre" value={this.state.cedula} name="cedula" />
+                        </div>
+                        <button type="submit" className="btn btn-outline-primary">Registrar</button>  
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
