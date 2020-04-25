34ff8ea137117d4b8fbf04441b55be5e1a3db4e6
Nilson28 <nilson.cardona@correounivalle.edu.co>
2020-04-24 21:44:03 -0500

    se agrego home al package

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Asignacion from './pages/Asignacion';
 import interfazUs from './pages/interfazUs';
 
 
-import * as serviceWorker from './serviceWorker';
+
 
 const history = createBrowserHistory();
 
@@ -30,7 +30,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
